529f0828a6ccfec6a05225b0ffa0621440a30a25
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-10-14 03:36:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from PIL import Image, ImageChops
 if __name__ == '__main__':
     #Изначальное видео
     clipname = "input.mp4"
+    resultFileName="output.mp4"
     clip = VideoFileClip(clipname)
     #Референсный кадр
     clip.save_frame("ref.png", t=2)
     ref = Image.open("ref.png")
     reference=ref.resize([4,4])
     #Извлечение первичного видео
-    ffmpeg_extract_subclip(clipname, 0.0, 1, targetname="result.mp4")
+    ffmpeg_extract_subclip(clipname, 0.0, 1, targetname=resultFileName)
     len=clip.duration
     print(len)
     i=1
-    final = VideoFileClip("result.mp4")
-    withmen=VideoFileClip("withmen.mp4")
+    #чем меньше a, тем выше точность вырезки и тем медленнее программа
+    a=0.5
+    final = VideoFileClip(resultFileName)
     flag=False
-    while i!=len-1:
+    while i<len-a:
         try:
             #Вырезается фрагмент
-            ffmpeg_extract_subclip(clipname, i, i+0.5, targetname="cut.mp4")
+            ffmpeg_extract_subclip(clipname, i, i+a, targetname="cut.mp4")
             partial=VideoFileClip("cut.mp4")
             #Из фрагмента берется кадр
             partial.save_frame("comp.png")
@@ -33,10 +35,10 @@ if __name__ == '__main__':
             if comparison == None:
                 #приклеивание части, в которой нет человека, к результирующему видео
                 final = concatenate_videoclips([final,partial])
-            i+=0.5
+            i+=a
         except Exception:
-            final.write_videofile("result.mp4")
-            i=len-1
+            final.write_videofile(resultFileName)
+            i=len-a
             flag=True
-    if flag==False:            
-        final.write_videofile("result.mp4")
+    if flag==False:
+        final.write_videofile(resultFileName)
